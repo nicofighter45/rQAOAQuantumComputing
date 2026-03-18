073796de6dd218c10dd5538aa4e39905ca4fcda1
nicofighter45 <fagot.nicolas.45@gmail.com>
2026-03-18 16:35:56 +0100

    Reworking graph implementation with nx library

--- instance_generator.py
+++ instance_generator.py
@@ -6,33 +6,8 @@ import matplotlib.pyplot as plt
 class Graph(nx.Graph):
     """A simple non oriented graph class to represent the problem instance."""
 
-    def __init__(self, num_verticles: int):
-        self.verticles = list(range(num_verticles))
-        self.edges = {i: set() for i in range(num_verticles)}
-
-    def number_of_edges(self) -> int:
-        """
-        Returns the number of edges in the graph.
-        """
-        return sum(len(edges) for edges in self.edges.values())
-
-    def number_of_verticles(self) -> int:
-        """
-        Returns the number of vertices in the graph.
-        """
-        return len(self.verticles)
-
-    def add_edge(self, i: int, j: int) -> None:
-        """
-        Add the edge i-j to the graph
-        """
-        if not 0 <= i < self.number_of_verticles() or not 0 <= j < self.number_of_verticles():
-            raise ValueError("Node indices must be within the range of the graph's vertices.")
-        
-        i, j = min(i, j), max(i, j)
-
-        if not j in self.edges[i]:
-            self.edges[i].add(j)
+    def __init__(self):
+        super().__init__()
 
     def draw(self) -> None:
             G = nx.Graph()
@@ -60,10 +35,10 @@ class Graph(nx.Graph):
 class ColoredGraph(Graph):
     """A simple non oriented colored graph class to represent the problem instance."""
 
-    def __init__(self, num_verticles: int, number_of_color: int):
-        super().__init__(num_verticles)
+    def __init__(self, number_of_color: int):
+        super().__init__()
         self.max_color = number_of_color - 1
-        self.coloration = [0 for _ in range(self.num_verticles)]
+        self.coloration = [0 for _ in range(len(self))]
     
     def set_color(self, color: int) -> None:
         if color < 0 or color > self.max_color:
@@ -71,7 +46,7 @@ class ColoredGraph(Graph):
         self.color = color
     
     def set_coloration(self, coloration: list[int]) -> None:
-        if len(coloration) != self.num_verticles():
+        if len(coloration) != len(self):
             raise ValueError("Coloration length must be equal to the number of vertices.")
         if any(c >= self.max_color or c < 0 for c in coloration):
             raise ValueError(f"Color values must be between 0 and {self.max_color}.")
