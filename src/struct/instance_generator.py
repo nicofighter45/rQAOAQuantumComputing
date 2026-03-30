bf441248d0041ce8dbb7bca027598927d057ed42
nicofighter45 <fagot.nicolas.45@gmail.com>
2026-03-30 09:20:55 +0200

    Adapt graph instance for weight and color

--- src/struct/instance_generator.py
+++ src/struct/instance_generator.py
@@ -6,26 +6,46 @@ import matplotlib.pyplot as plt
 def random_graph(n: int, p: float) -> Graph:
     G = nx.erdos_renyi_graph(n=n, p=p)
     G.__class__ = Graph
+    for u, v in G.edges():
+        G[u][v]['weight'] = 1
+        G[u][v]['color'] = 1
     return G
+
+def int_to_color(i: int) -> str:
+    colors = ["red", "blue", "green", "yellow", "cyan", "magenta", "orange", "purple"]
+    return colors[i % len(colors)]
+
 
 class Graph(nx.Graph):
     """A simple non oriented graph class to represent the problem instance."""
 
     def __init__(self):
         super().__init__()
+        self.color_list = None
+    
+    def set_color(self, u: int, v: int, color: int) -> None:
+        self[u][v]['color'] = color
+    
+    def set_weight(self, u: int, v: int, weight: float) -> None:
+        self[u][v]['weight'] = weight
+
+    @property
+    def color_list(self) -> list[int]:
+        return [int_to_color(self[u][v]['color']) for u, v in self.edges()]
+    
+    @property
+    def weight_list(self) -> list[float]:
+        return [self[u][v]['weight'] for u, v in self.edges()]
 
     def draw(self) -> None:
-            G = nx.Graph()
-            G.add_nodes_from(range(len(self)))
-            G.add_edges_from(self.edges)
 
             plt.figure(figsize=(3, 3))
             
-            pos = nx.spring_layout(G, seed=42)
+            pos = nx.spring_layout(self, seed=42)
 
-            nx.draw_networkx_nodes(G, pos, node_size=800, node_color="#b3d9ff", edgecolors="black")
-            nx.draw_networkx_edges(G, pos)
-            nx.draw_networkx_labels(G, pos)
+            nx.draw_networkx_nodes(self, pos, node_size=800, node_color=self.color_list, edgecolors="black")
+            nx.draw_networkx_edges(self, pos)
+            nx.draw_networkx_labels(self, pos)
             
             plt.title(f"Graph |V|={len(self)}, |E|={len(self.edges)}")
             plt.axis("off")
@@ -34,18 +54,18 @@ class Graph(nx.Graph):
 
     
     def __str__(self) -> str:
-        return f"Graph with {len(self.verticles)} vertices and edges: {self.edges}"
+        return f"Graph with {len(self)} vertices and edges: {self.edges}"
 
 
 class ProblemInstance:
     """Instance of a graph coloring problem"""
-    def __init__(self, graph, k):
+    def __init__(self, graph: Graph, k: int):
         self.graph = graph
         self.k = k
         self.hamiltonian = Hamiltonian(graph)
 
 class AbstractSolverInstance(abc.ABC):
-    def __init__(self, graph, number_of_color):
+    def __init__(self, graph: Graph, number_of_color: int):
         self.graph = graph
         self.number_of_color = number_of_color
 
